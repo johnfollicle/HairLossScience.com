819bc3b87b1a83a9eff11fbc48dc05651d79ffb3
johnfollicle <folliclefacts@gmail.com>
2024-07-28 17:39:21 +0200

    Switch to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import TreatmentComparison from "./TreatmentComparison";
 import TreatmentDetails from "./TreatmentDetails";
 
